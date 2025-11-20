90cf2386ab7b3b473074fb951f8c20c44b9f9f85
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2025-11-20 12:10:40 -0800

    Added health and energy

--- organism/Body.py
+++ organism/Body.py
@@ -5,6 +5,16 @@ Creates a simple organism for prototyping purposes
 from utilities import *
 import Chemicals
 import random
+import numpy as np
+
+ORGAN_ENERGY_MULTIPLIER = .4
+HEALTH_DEATH_THRESHOLD = .05
+
+def health_function(val):
+    """
+    Maps organ health to overall creature health. Low health organs are very detrimental.
+    """
+    return 1/(1+(100*(np.e** (-10 * val))))
 
 class Body:
     """
@@ -19,10 +29,14 @@ class Body:
         self._chems = {chem: 0 for chem in Chemicals.CHEMS}
         self._concentrations = {chem: 0 for chem in Chemicals.CHEMS}
         self._organs = []
+        self._health = 1
+        self._alive = True
         self._brain = None
         self._genome = genome
         self._dna_head = None
         self._heading = [1,0]
+        self._energy = 1
+        self._max_energy = 1
 
     def set_grid(self, grid):
         self._grid = grid
@@ -40,6 +54,7 @@ class Body:
         """
         Adds a new organ to the creature
         """
+        self._max_energy += organ.get_energy_capacity()*ORGAN_ENERGY_MULTIPLIER
         self._organs.append(organ)
 
     def activate_organs(self):
@@ -52,6 +67,13 @@ class Body:
                 organ.activate_organ()
         self.calc_concentrations()
 
+    def get_max_energy(self):
+        tot = 1
+        for organ in organs:
+            if organ.get_health() > 0:
+                tot += organ.get_energy_capacity()*ORGAN_ENERGY_MULTIPLIER
+        self._max_energy = tot
+        
     def calc_concentrations(self):
         """
         Rechecks the concentrations of chemicals in the body
@@ -122,11 +144,35 @@ class Body:
         for chem in chems:
             self.add_chemical(chem, chems[chem].get_quantity())
         energy = food.get_energy()
-        self._add_energy(energy)
-    
+        self.add_energy(energy)
+
+    def add_energy(self, amount):
+        self._energy = min(self._max_energy, self._energy + amount)
+
+    def remove_energy(self, amount):
+        self._energy -= amount
+        
     def get_organs(self):
         return self._organs
-
+    
+    def check_alive(self):
+        self.check_organ_health()
+        if self._health < HEALTH_DEATH_THRESHOLD:
+            return False
+        if self._energy <= 0:
+            return False
+        return True
+    
+    def check_organ_health(self):
+        o_healths = 0
+        for organ in self._organs:
+            o_health = organ.get_health()
+            if o_health < HEALTH_DEATH_THRESHOLD:
+                self._organs.remove(organ)
+            else:
+                o_healths += health_function(o_health)
+        self._health = o_healths/len(self._organs)
+        
     def describe(self):
         print(f"Creature {self._id}:")
         for organ in self._organs:
